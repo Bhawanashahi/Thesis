9ecf6116f93fd956144c47d34d271189c07f3a0b
Bhawana shahi <shahivawana61@gmail.com>
2024-08-09 23:11:14 +0545

    deleted unwanted

--- Ecommerce-world/client/src/reducers/drawerReducer.js
+++
@@ -1,8 +0,0 @@
-export const drawerReducer = (state = false, action) => {
-    switch(action.type){
-        case "DRAWER_VISIBILITY":
-            return action.payload
-        default:
-            return state
-    }
-}
--- Ecommerce-world/client/src/reducers/pageStateReducer.js
+++
@@ -1,8 +0,0 @@
-export const pageStateReducer = (state = false, action) => {
-    switch(action.type){
-        case "SESSION":
-            return action.payload
-        default:
-            return state
-    }
-}
